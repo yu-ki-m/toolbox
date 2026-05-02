cf243a6b6479945fd21662e17d53963d8022e151
Yuki <96922097+yu-ki-m@users.noreply.github.com>
2026-05-02 11:32:17 +0900

    Delete canvas-editor directory

--- canvas-editor/assets/chunk-55IACEB6-BJMI6_WW.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./workspace-vendors-DUHxtseO.js";import{n as t}from"./src-B07MgS1v.js";var n=t((t,n)=>{let r;return n===`sandbox`&&(r=e(`#i`+t)),e(n===`sandbox`?r.nodes()[0].contentDocument.body:`body`).select(`[id="${t}"]`)},`getDiagramElement`);export{n as t};
--- canvas-editor/assets/gitGraph-7Q5UKJZL-D_oOF74S.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-K5T4RW27-DxON_Yp9.js";import{n as e}from"./chunk-67CJDMHE-UH6cAiwd.js";export{e as createGitGraphServices};
